a6aa4d9d4b38aaf3d191c127dd8ee7efb6eeda35
Frederic Heem <frederic.heem@gmail.com>
2021-03-25 12:57:47 -0500

    Deploy website - based on c3d2de71be85d18d2d2ec8379a26b9e7683adea1

---
+++ 103.f58b7dcd.js
@@ -0,0 +1,18 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[103],{229:function(e,t,n){"use strict";var a=n(2),o=n(209),c=n(208),r=n(206),l=n(204),i=n(207);n(93);function s({to:e,href:t,label:n,prependBaseUrlToHref:c,...l}){const s=Object(i.a)(e),b=Object(i.a)(t,{forcePrependBaseUrl:!0});return Object(o.b)(r.a,Object(a.a)({className:"footer__link-item"},t?{target:"_blank",rel:"noopener noreferrer",href:c?b:t}:{to:s},l),n)}t.a=function(){const e=Object(l.a)(),{siteConfig:t={}}=e,{themeConfig:n={}}=t,{footer:a}=n,{copyright:r,links:i=[]}=a||{};return Object(o.b)("footer",{className:Object(c.a)("footer",{"footer--dark":"dark"===a.style})},Object(o.b)("div",null,Object(o.b)("div",{css:o.a`
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-content: center;
+          `},i.map((e,t)=>Object(o.b)("div",{css:o.a`
+                margin: 20px;
+              `,key:t},null!=e.title?Object(o.b)("h4",{className:"footer__title"},e.title):null,Object(o.b)("ul",{css:o.a`
+                  padding: 0 30px 0 00px;
+                  list-style: none;
+                `},e.items.map((e,t)=>Object(o.b)("li",{key:e.href||e.to},Object(o.b)(s,e))))))),Object(o.b)("div",{css:o.a`
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            > div {
+              padding-right: 30px;
+            }
+          `},Object(o.b)("div",{dangerouslySetInnerHTML:{__html:r}}),Object(o.b)("a",{href:"https://www.freeprivacypolicy.com/live/ada70892-c887-4a1d-8d0e-29801fe215b2"},"Privacy Policy"))))}},268:function(e,t,n){"use strict";n.r(t);var a=n(0),o=n.n(a),c=n(226);t.default=function(){return o.a.createElement(c.a,{title:"Page Not Found"},o.a.createElement("div",{className:"container margin-vert--xl"},o.a.createElement("div",{className:"row"},o.a.createElement("div",{className:"col col--6 col--offset-3"},o.a.createElement("h1",{className:"hero__title"},"Page Not Found"),o.a.createElement("p",null,"We could not find what you were looking for."),o.a.createElement("p",null,"Please contact the owner of the site that linked you to the original URL and let them know their link is broken.")))))}}}]);
