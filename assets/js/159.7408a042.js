a30e769e6b417fefe46b45f15b61e43293238e2c
Frederic Heem <frederic.heem@gmail.com>
2021-09-03 14:00:03 -0300

    Deploy website - based on 3e780019f7f6c6bada7b1292f739cc6891bd6458

---
+++ assets/js/159.7408a042.js
@@ -0,0 +1,18 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[159],{256:function(e,t,a){"use strict";var n=a(3),o=a(237),r=a(231),c=a(234),i=a(16),l=a(235);a(57);function s({to:e,href:t,label:a,prependBaseUrlToHref:r,...i}){const s=Object(l.a)(e),d=Object(l.a)(t,{forcePrependBaseUrl:!0});return Object(o.b)(c.a,Object(n.a)({className:"footer__link-item"},t?{target:"_blank",rel:"noopener noreferrer",href:r?d:t}:{to:s},i),a)}t.a=function(){const e=Object(i.default)(),{siteConfig:t={}}=e,{themeConfig:a={}}=t,{footer:n}=a,{copyright:c,links:l=[]}=n||{};return Object(o.b)("footer",{className:Object(r.a)("footer",{"footer--dark":"dark"===n.style})},Object(o.b)("div",null,Object(o.b)("div",{css:o.a`
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-content: center;
+          `},l.map(((e,t)=>Object(o.b)("div",{css:o.a`
+                margin: 20px;
+              `,key:t},null!=e.title?Object(o.b)("h4",{className:"footer__title"},e.title):null,Object(o.b)("ul",{css:o.a`
+                  padding: 0 30px 0 00px;
+                  list-style: none;
+                `},e.items.map(((e,t)=>Object(o.b)("li",{key:e.href||e.to},Object(o.b)(s,e))))))))),Object(o.b)("div",{css:o.a`
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            > div {
+              padding-right: 30px;
+            }
+          `},Object(o.b)("div",{dangerouslySetInnerHTML:{__html:c}}),Object(o.b)("a",{href:"https://www.freeprivacypolicy.com/live/ada70892-c887-4a1d-8d0e-29801fe215b2"},"Privacy Policy"))))}},292:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),r=a(255),c=a(233);t.default=function(){return o.a.createElement(r.a,{title:"Page Not Found"},o.a.createElement("main",{className:"container margin-vert--xl"},o.a.createElement("div",{className:"row"},o.a.createElement("div",{className:"col col--6 col--offset-3"},o.a.createElement("h1",{className:"hero__title"},o.a.createElement(c.a,{id:"theme.NotFound.title",description:"The title of the 404 page"},"Page Not Found")),o.a.createElement("p",null,o.a.createElement(c.a,{id:"theme.NotFound.p1",description:"The first paragraph of the 404 page"},"We could not find what you were looking for.")),o.a.createElement("p",null,o.a.createElement(c.a,{id:"theme.NotFound.p2",description:"The 2nd paragraph of the 404 page"},"Please contact the owner of the site that linked you to the original URL and let them know their link is broken."))))))}}}]);
